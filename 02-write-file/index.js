cc114bed6b64dee46045850a1be5fd78000f9668
rinnnnsh <irina_shaman@mail.ru>
2022-11-03 18:14:35 +0300

    feat: add 03 solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,25 +4,29 @@ const readline = require('readline');
 const { stdin, stdout } = process;
 
 let writeableStream = fs.createWriteStream(
-    path.join(__dirname, 'text.txt'), 'utf-8'
+  path.join(__dirname, 'text.txt'), 'utf-8'
 );
 console.log('Введите текст: ');
 
 const message = readline.createInterface({
-    input: stdin,
-    output: stdout,
+  input: stdin,
+  output: stdout,
 });
+
 message.on('line', (line) => {
-    if (line === 'exit') {
-        message.close();
-    } else {
-        writeableStream.write(line + '\n');
-    }
+  if (line === 'exit') {
+    message.close();
+  } else {
+    writeableStream.write(line + '\n');
+  }
 });
 message.on('SIGINT', () => {
-    message.close();
+  message.close();
 });
+
 message.on('close', () => {
-    writeableStream.write(message.line);
-    stdout.write('Ввод данных завершен.\n');
+  writeableStream.write(message.line);
+  stdout.write('Ввод данных завершен.\n');
 });
+
+
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+ 
+const filePath = path.resolve(__dirname, 'secret-folder');
+ 
+fs.readdir(filePath, function(err, items) {
+  for (const item of items) {
+    const itemInfo = path.parse(item);
+    let itemResult = `${itemInfo.name} - ${(itemInfo.ext).slice(1)} - `;
+
+    fs.stat(`${filePath}\\${item}`, (err, stats) => {
+      if (stats.isFile()) {
+        itemResult += `${stats.size}kb`;
+        console.log(itemResult);
+      }
+    });   
+  }
+});
